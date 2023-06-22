18762ae0475aff1aee0bc4b940ee73f90f7ec097
tpapak01 <tpapak01@cs.ucy.ac.cy>
2023-06-22 10:37:13 +0300

    Transformed custom MOEAD to original MOEAD

--- problems/CostDistr.java
+++ problems/CostDistr.java
@@ -99,7 +99,7 @@ public class CostDistr extends Problem {
         try {
             if (this.lowerLevelAlgorithmName.equals("MOEAD"))
                 lowerLevelSolutions = LowerLevelMOKP_MOEAD.evaluate(costs);
-            else lowerLevelSolutions = LowerLevelMOKP_NSGAII.evaluate(new XReal(solution));
+            else lowerLevelSolutions = LowerLevelMOKP_NSGAII.evaluate(costs);
         } catch (Exception e){
             System.out.println("Exception at LowerLevelMOKP.evaluate: " + e.getMessage());
         }
