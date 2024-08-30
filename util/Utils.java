2abb5a029aa105357e81c52d661d47b070e7002d
tpapak01 <tpapak01@cs.ucy.ac.cy>
2024-08-30 16:32:12 +0300

    Move STD to UTILS
    (cherry picked from commit ef092c83c541daed86241e789d3176d572720ae3)

--- util/Utils.java
+++ util/Utils.java
@@ -345,6 +345,27 @@ public class Utils {
 		return fitness;
 	} // fitnessEvaluation
 
+	public static double calculateStandardDeviation(double[] array) {
+
+		// get the sum of array
+		double sum = 0.0;
+		for (double i : array) {
+			sum += i;
+		}
+
+		// get the mean of array
+		int length = array.length;
+		double mean = sum / length;
+
+		// calculate the standard deviation
+		double standardDeviation = 0.0;
+		for (double num : array) {
+			standardDeviation += Math.pow(num - mean, 2);
+		}
+
+		return Math.sqrt(standardDeviation / length);
+	}
+
 
 
 }
