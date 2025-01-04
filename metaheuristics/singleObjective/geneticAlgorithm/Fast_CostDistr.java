1d9bb27b3eeb476aec8aca27e7077678a7f9c90d
tpapak01 <tpapak01@gmail.com>
2025-01-04 21:04:30 +0200

    Choose by argument the data and results destination (server or local PC)
    (cherry picked from commit 8a26889fa0ea1d4566e42a8add9f3ce322aa7254)

--- metaheuristics/singleObjective/geneticAlgorithm/Fast_CostDistr.java
+++ metaheuristics/singleObjective/geneticAlgorithm/Fast_CostDistr.java
@@ -40,6 +40,7 @@ public class Fast_CostDistr extends Algorithm {
   private int populationSize;
   private SolutionSet population;
   int evaluations;
+  private static String problemPath = "C:\\Users\\emine\\source\\repos\\SmartHome3\\SmartHome3\\wwwroot\\";
 
  /**
   *
@@ -47,9 +48,10 @@ public class Fast_CostDistr extends Algorithm {
   * Create a new GGA instance.
   * @param problem Problem to solve.
   */
-  public Fast_CostDistr(Problem problem){
+  public Fast_CostDistr(Problem problem, String dataPath){
     super(problem);
     problemCostDistr = (CostDistr) problem;
+    if (!dataPath.equals("-")) { problemPath = dataPath; }
   } // GGA
   
  /**
