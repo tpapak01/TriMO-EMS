7f1da1963f547ee84277fa7774d2ed7aa5e9df9a
tpapak01 <tpapak01@gmail.com>
2025-09-27 14:13:42 +0300

    Add ADAPTIVE DE

---
+++ metaheuristics/bilevel/UpperLevelCostDistr_AdaptiveDE.java
@@ -0,0 +1,161 @@
+package jmetal.metaheuristics.bilevel;
+
+import jmetal.core.Algorithm;
+import jmetal.core.Operator;
+import jmetal.core.Problem;
+import jmetal.core.SolutionSet;
+import jmetal.metaheuristics.singleObjective.geneticAlgorithm.AdaptiveDE_CostDistr;
+import jmetal.operators.crossover.CrossoverFactory;
+import jmetal.operators.selection.Selection;
+import jmetal.operators.selection.SelectionFactory;
+import jmetal.problems.CostDistr;
+import jmetal.problems.MOKP_Problem;
+import jmetal.util.JMException;
+import jmetal.util.comparators.ObjectiveComparator;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+
+public class UpperLevelCostDistr_AdaptiveDE {
+
+
+    public static Logger logger_;      // Logger object
+    public static FileHandler fileHandler_; // FileHandler object
+    private static String problemPath = "C:\\Users\\emine\\IdeaProjects\\MOEAD test project\\";
+
+    /**
+     * @param args Command line arguments. The first (optional) argument specifies
+     *             the problem to solve.
+     * @throws JMException
+     * @throws IOException
+     * @throws SecurityException      Usage: three options
+     *                                - jmetal.metaheuristics.moead.MOEAD_main
+     *                                - jmetal.metaheuristics.moead.MOEAD_main problemName
+     *                                - jmetal.metaheuristics.moead.MOEAD_main problemName ParetoFrontFile
+     * @throws ClassNotFoundException
+     */
+    public static void main(String[] args) throws JMException, SecurityException, IOException, ClassNotFoundException {
+
+        Problem problem;         // The problem to solve
+        Algorithm algorithm;         // The algorithm to use
+        Operator crossover;         // Crossover operator
+
+        String problemName = args[0];
+        String problemUserPreferences = args[1];
+        String renewableName = args[2];
+        String lowerLevelAlgorithmName = args[3];
+        String costsName = "-";
+        if (args.length > 4)
+            costsName = args[4];
+        String dataPath = "-";
+        if (args.length > 5) {
+            dataPath = args[5];
+            if (!dataPath.equals("-")) { problemPath = dataPath; dataPath += "data\\"; }
+        }
+        String paretoFileName = "-";
+        if (args.length > 6)
+            paretoFileName = args[6];
+        //initialize Lower Level algorithm
+        MOKP_Problem lowerLevelProblem = new MOKP_Problem(problemName, problemUserPreferences, dataPath);
+        if (lowerLevelAlgorithmName.equals("MOEAD"))
+            LowerLevelMOKP_MOEAD.initializeAlgorithm(lowerLevelProblem, dataPath, paretoFileName);
+        else
+            LowerLevelMOKP_NSGAII.initializeAlgorithm(lowerLevelProblem, dataPath, paretoFileName);
+
+        //thalis
+        problem = new CostDistr(renewableName, lowerLevelProblem, lowerLevelAlgorithmName, costsName, dataPath);
+
+        algorithm = new AdaptiveDE_CostDistr(problem, problemPath); // Generational GA
+
+        /* Algorithm parameters*/
+        algorithm.setInputParameter("populationSize", 100); //must be even number
+        if (costsName.equals("-"))
+            algorithm.setInputParameter("maxEvaluations", 1000000);
+        else
+            algorithm.setInputParameter("maxEvaluations", 100);
+
+
+        //thalis
+        algorithm.setInputParameter("dataDirectory",
+                "/Users/emine/IdeaProjects/JMETALHOME/data/MOEAD_parameters/Weight");
+
+        /* Crossover operator */
+        HashMap parameters = new HashMap() ;
+        parameters.put("CR", 0.5);
+        parameters.put("F", 0.5);
+        parameters.put("K", 0.5);
+        parameters.put("DE_VARIANT", "rand/1/bin");
+        crossover = CrossoverFactory.getCrossoverOperator("DifferentialEvolutionCrossover", parameters);
+
+        /* Selection Operator */
+        parameters = null;
+        Selection selection = SelectionFactory.getSelectionOperator("DifferentialEvolutionSelection", parameters) ;
+
+        /* Comparator */
+        Comparator comparator;
+        if (problem.isMaxmized())
+            comparator = new ObjectiveComparator(0, true) ; // Single objective comparator
+        else comparator = new ObjectiveComparator(0) ; // Single objective comparator
+        algorithm.setInputParameter("comparator", comparator);
+
+        /* Add the operators to the algorithm*/
+        algorithm.addOperator("crossover", crossover);
+        algorithm.addOperator("selection", selection);
+
+        /* Execute the Algorithm */
+        long initTime = System.currentTimeMillis();
+        SolutionSet population = algorithm.execute();
+        long estimatedTime = System.currentTimeMillis() - initTime;
+        System.out.println("Total execution time: " + estimatedTime);
+
+        /* Log messages */
+        System.out.println("Objectives values have been writen to file FUN");
+        population.printObjectivesToFile("FUN");
+        System.out.println("Variables values have been writen to file VAR");
+        population.printMyVariablesToFile("VAR", true);
+        population.printMyVariablesToFile(problemPath + "results\\Winner\\Prices", false);
+
+        population.printSelfConsumptionToFile("SELF_CONSU", true);
+        population.printSelfConsumptionToFile(problemPath + "results\\Winner\\SELF_CONSU", false);
+        population.printNonREPaidToFile("NON_RE_PAID");
+        population.printLowerLevelVarsToFile("LL_VAR");
+        population.printLowerLevelObjToFile("LL_FUN", true);
+        population.printLowerLevelObjToFile(problemPath + "results\\Winner\\LL_FUN", false);
+        population.printMappingToFile("LL_MAPPING");
+
+        population.printSpentEnergyToFile("SPENT");
+        population.printUserDissatisfactionToFile("USER_DISSAT", true);
+        population.printUserDissatisfactionToFile(problemPath + "results\\Winner\\USER_DISSAT", false);
+        population.printStdDevUserDissatisfactionToFile("STDDEV_USER_DISSAT");
+        population.printUserEnergyToFile("USER_ENERGY");
+        population.printUserEnergyToFile(problemPath + "results\\Winner\\USER_COSTS");
+        population.printStdDevUserEnergyToFile("STDDEV_USER_ENERGY");
+
+        FileWriter timeWriter = new FileWriter("TIME", true);
+        timeWriter.write(estimatedTime + "\n");
+        timeWriter.close();
+
+        //SolutionSet specialPareto = population.get(0).getLL_Transfer_pop();
+        //specialPareto.printObjectivesToFile("LowerLevelParetoVisual/" + "0_FUN"); //check
+
+
+        SolutionSet lowerLevelSolutions = population.get(0).getLL_Pareto_pop();
+        lowerLevelSolutions.printParetoToFile("LL_PARETO", true);
+        System.out.println(population.get(0).getUL_Optimism());
+        //Platform
+        lowerLevelSolutions.printParetoToFile(problemPath + "results\\Winner\\Pareto", false);
+        lowerLevelSolutions.printSpentsToFile(problemPath + "results\\Spents\\");
+        lowerLevelSolutions.printSelfsToFile(problemPath + "results\\Selfs\\");
+        lowerLevelSolutions.printUserDissatsToFile(problemPath + "results\\UserDissats\\");
+        lowerLevelSolutions.printUserCostsToFile(problemPath + "results\\UserCosts\\");
+        //population.printSpentEnergyToFile(platformPath + "Winner\\SPENT");
+
+
+
+    } //main
+
+}
---
+++ metaheuristics/singleObjective/geneticAlgorithm/AdaptiveDE_CostDistr.java
@@ -0,0 +1,270 @@
+//  gGA.java
+//
+//  Author:
+//       Antonio J. Nebro <antonio@lcc.uma.es>
+//       Juan J. Durillo <durillo@lcc.uma.es>
+//
+//  Copyright (c) 2011 Antonio J. Nebro, Juan J. Durillo
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+// 
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package jmetal.metaheuristics.singleObjective.geneticAlgorithm;
+
+import jmetal.core.*;
+import jmetal.encodings.variable.ArrayReal;
+import jmetal.metaheuristics.moead.MOEAD;
+import jmetal.operators.crossover.DifferentialEvolutionCrossover;
+import jmetal.problems.CostDistr;
+import jmetal.util.EuclideanDist;
+import jmetal.util.JMException;
+import jmetal.util.PseudoRandom;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Comparator;
+import java.util.HashMap;
+
+/** 
+ * Class implementing a generational genetic algorithm
+ */
+public class AdaptiveDE_CostDistr extends Algorithm {
+
+  private CostDistr problemCostDistr;
+  private int populationSize;
+  private SolutionSet population;
+  int evaluations;
+  private static String problemPath = "C:\\Users\\emine\\IdeaProjects\\MOEAD test project\\";
+
+  private static double[] Fpool = new double[] {0.4, 0.5, 0.6};
+  private static double[] CRpool = new double[] {0.7, 0.8, 0.9};
+  private static double[] Kpool = new double[] {0.4, 0.5, 0.6};
+
+ /**
+  *
+  * Constructor
+  * Create a new GGA instance.
+  * @param problem Problem to solve.
+  */
+  public AdaptiveDE_CostDistr(Problem problem, String dataPath){
+    super(problem);
+    problemCostDistr = (CostDistr) problem;
+    if (!dataPath.equals("-")) { problemPath = dataPath; }
+  } // GGA
+  
+ /**
+  * Execute the GGA algorithm
+ * @throws JMException 
+  */
+  public SolutionSet execute() throws JMException, ClassNotFoundException {
+
+    int maxEvaluations ;
+
+
+
+    Operator    mutationOperator  ;
+    Operator    crossoverOperator ;
+    Operator    selectionOperator ;
+    
+    Comparator  comparator        ;
+    
+    // Read the params
+    populationSize = ((Integer)this.getInputParameter("populationSize")).intValue();
+    maxEvaluations = ((Integer)this.getInputParameter("maxEvaluations")).intValue();                
+   
+    // Initialize the variables
+    population          = new SolutionSet(populationSize) ;
+    
+    evaluations  = 0;                
+
+    // Read the operators
+    comparator = (Comparator) this.getInputParameter("comparator");
+    mutationOperator  = this.operators_.get("mutation");
+    crossoverOperator = this.operators_.get("crossover");
+    selectionOperator = this.operators_.get("selection");  
+
+    initPopulation();
+    //double[] producedRE = problemCostDistr.getProducedRE();
+    //initPopulationCostDistr(producedRE);
+
+    // Sort population
+    population.sort(comparator) ;
+
+    //Convergence
+    int generations_left_for_convergence = 10;
+    int converged = generations_left_for_convergence;
+    double best_solution = population.get(0).getObjective(0);
+    System.out.println("BEST SOL: " + best_solution);
+
+    int iterations = populationSize/2;
+    int stop = 0;
+    int generation = 0;
+
+    int flag = PseudoRandom.randInt(1,3);
+
+    while (evaluations < maxEvaluations && converged != 0) {
+
+      HashMap parameters = new HashMap();
+      parameters.put("CR", CRpool[flag-1]);
+      parameters.put("F", Fpool[flag-1]);
+      parameters.put("K", Kpool[flag-1]);
+      //parameters.put("DE_VARIANT", "rand/1/bin");
+      ((DifferentialEvolutionCrossover) crossoverOperator).updateParameters(parameters);
+
+      Solution winner = population.get(0);
+      //System.out.println("q: " + winner.getUL_Optimism());
+
+      SolutionSet winnerPareto = winner.getLL_Pareto_pop();
+      winnerPareto.printParetoToFile(problemPath + "results\\Paretos\\PARETO_" + (generation), false);
+      SolutionSet onlyWinner = new SolutionSet(1);
+      onlyWinner.add(winner);
+      onlyWinner.printSelfConsumptionToFile(problemPath + "results\\SelfsInProgress\\SELF_" + (generation), false);
+
+      generation++;
+
+      SolutionSet offspringPopulation = new SolutionSet(populationSize) ;
+
+      double diff = Math.abs(winner.getObjective(0) - population.get(populationSize-1).getObjective(0));
+      if (diff <= 10 ) {
+        MOEAD.conv = 0.001;
+      }
+      if (diff <= 5 ) {
+        MOEAD.conv = 0.0001;
+      }
+
+      // Reproductive cycle: keep adding 2 offspring to the offspring population until it reaches the max size
+      for (int i = 0 ; i < iterations; i++) {
+
+        Solution current = population.get(i);
+
+        // Selection: 3 random parents
+        // Two parameters are required: the population and the index of the current individual
+        Solution[] tempParents = (Solution [])selectionOperator.execute(new Object[]{population, i});
+        Solution[] parents = new Solution[4];
+        parents[0] = tempParents[0]; parents[1] = tempParents[1]; parents[2] = tempParents[2];
+        parents[3] = winner;
+
+        // Crossover.
+        // Two parameters are required: the current individual and the array of parents
+        Solution offspring = (Solution)crossoverOperator.execute(new Object[]{current, parents}) ;
+
+        //attach to offspring the LL ND of the closest UL solution
+        double min_dist = Double.MAX_VALUE;
+        int min_index = -1;
+        for (int o = 0; o<2; o++) {
+          Double[] offspringVar = (((ArrayReal) offspring.getDecisionVariables()[0]).array_);
+          for (int j = 0; j < populationSize; j++) {
+            Double[] popSolutionVar = (((ArrayReal) population.get(j).getDecisionVariables()[0]).array_);
+            double dist = EuclideanDist.distance(offspringVar, popSolutionVar);
+            if (dist < min_dist){
+              min_dist = dist;
+              min_index = j;
+            }
+          }
+          offspring.setLL_Transfer_pop(population.get(min_index).getLL_Transfer_pop());
+        }
+
+        // Evaluation of the new individuals
+        problem_.evaluate(offspring);
+        evaluations++;
+
+        // if child better than currently-examined individual, replace him
+        if (comparator.compare(current, offspring) < 0)
+          offspringPopulation.add(current);
+        else
+          offspringPopulation.add(offspring);
+
+        if (stop == 1){
+          break;
+        }
+
+      } // for
+      
+      // The offspring population is added to the new current population
+      SolutionSet popCombined = population.union(offspringPopulation);
+      offspringPopulation.clear();
+      population.clear();
+      popCombined.sort(comparator);
+      //TODO add here more than just sorting....
+      for (int i = 0; i < populationSize; i++) {
+        population.add(popCombined.get(i)) ;
+      }
+
+      if (stop == 1){
+        break;
+      }
+
+      winner = population.get(0);
+      //check for convergence
+      if (winner.getObjective(0) >= best_solution){
+        converged--;
+        flag = PseudoRandom.randInt(1,3);
+      } else {
+        best_solution = winner.getObjective(0);
+        System.out.println("BEST SOL at " + evaluations + ": " + best_solution);
+        converged = generations_left_for_convergence;
+      }
+
+    } // while
+    
+    // Return a population with the best individual
+    SolutionSet resultPopulation = new SolutionSet(1) ;
+    resultPopulation.add(population.get(0)) ;
+    
+    System.out.println("Evaluations: " + evaluations ) ;
+    FileWriter evalsWriter = null;
+    try {
+      evalsWriter = new FileWriter("EVALS", true);
+      evalsWriter.write(evaluations + "\n");
+      evalsWriter.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    return resultPopulation ;
+  } // execute
+
+  /**
+   *
+   */
+  public void initPopulation() throws JMException, ClassNotFoundException {
+    if (problemCostDistr.getInputCosts() == null) {
+      for (int i = 0; i < populationSize; i++) {
+        Solution newSolution = new Solution(problem_, true);
+
+        problem_.evaluate(newSolution);
+        evaluations++;
+        population.add(newSolution);
+      } // for
+    } else {
+      for (int i = 0; i < populationSize; i++) {
+        Solution newSolution = new Solution(problem_, true);
+        newSolution.setDecisionVariables(updateSolution(problemCostDistr.getInputCosts()));
+        problem_.evaluate(newSolution);
+        evaluations++;
+        population.add(newSolution);
+      } // for
+    }
+  } // initPopulation
+
+  public Variable[] updateSolution(double[] costsToSend) throws JMException {
+    Variable [] variables = new Variable[1];
+    ArrayReal arrayReal = new ArrayReal(problem_.getNumberOfVariables(), problem_);
+    for (int j=0; j<problem_.getNumberOfVariables(); j++)
+      arrayReal.setValue(j, costsToSend[j]);
+    variables[0] = arrayReal;
+    return variables;
+  }
+
+
+} // gGA
