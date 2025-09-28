8fb868eae0e6617b4688a445475d208ff3f1b2b4
tpapak01 <tpapak01@gmail.com>
2025-09-28 15:34:29 +0300

    Add mutation to ADE

--- metaheuristics/bilevel/UpperLevelCostDistr_AdaptiveDE.java
+++ metaheuristics/bilevel/UpperLevelCostDistr_AdaptiveDE.java
@@ -6,6 +6,7 @@ import jmetal.core.Problem;
 import jmetal.core.SolutionSet;
 import jmetal.metaheuristics.singleObjective.differentialEvolution.AdaptiveDE_CostDistr;
 import jmetal.operators.crossover.CrossoverFactory;
+import jmetal.operators.mutation.UniformMutation;
 import jmetal.operators.selection.Selection;
 import jmetal.operators.selection.SelectionFactory;
 import jmetal.problems.CostDistr;
@@ -43,6 +44,7 @@ public class UpperLevelCostDistr_AdaptiveDE {
         Problem problem;         // The problem to solve
         Algorithm algorithm;         // The algorithm to use
         Operator crossover;         // Crossover operator
+        Operator mutation;         // Mutation operator
 
         String problemName = args[0];
         String problemUserPreferences = args[1];
@@ -91,6 +93,14 @@ public class UpperLevelCostDistr_AdaptiveDE {
         parameters.put("DE_VARIANT", "rand/1/bin");
         crossover = CrossoverFactory.getCrossoverOperator("DifferentialEvolutionCrossover", parameters);
 
+        //thalis
+        Double perturbationIndex = 0.5;
+        Double mutationProbability = 1.0 / problem.getNumberOfVariables();
+        parameters = new HashMap();
+        parameters.put("probability", mutationProbability);
+        parameters.put("perturbation", perturbationIndex);
+        mutation = new UniformMutation(parameters);
+
         /* Selection Operator */
         parameters = null;
         Selection selection = SelectionFactory.getSelectionOperator("DifferentialEvolutionSelection", parameters) ;
@@ -105,6 +115,7 @@ public class UpperLevelCostDistr_AdaptiveDE {
         /* Add the operators to the algorithm*/
         algorithm.addOperator("crossover", crossover);
         algorithm.addOperator("selection", selection);
+        algorithm.addOperator("mutation", mutation);
 
         /* Execute the Algorithm */
         long initTime = System.currentTimeMillis();
