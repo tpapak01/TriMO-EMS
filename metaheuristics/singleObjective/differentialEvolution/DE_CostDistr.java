3be07a50f57339e03c768a7ec6f3d1e84317054d
tpapak01 <tpapak01@gmail.com>
2025-11-15 14:23:47 +0200

    Fix normal DE for extra tests

--- metaheuristics/singleObjective/differentialEvolution/DE_CostDistr.java
+++ metaheuristics/singleObjective/differentialEvolution/DE_CostDistr.java
@@ -23,8 +23,10 @@ package jmetal.metaheuristics.singleObjective.differentialEvolution;
 
 import jmetal.core.*;
 import jmetal.encodings.variable.ArrayReal;
+import jmetal.metaheuristics.moead.MOEAD;
 import jmetal.problems.CostDistr;
 import jmetal.problems.MOKP_Problem;
+import jmetal.util.EuclideanDist;
 import jmetal.util.JMException;
 import jmetal.util.comparators.ObjectiveComparator;
 
@@ -38,21 +40,22 @@ import java.util.Comparator;
 public class DE_CostDistr extends Algorithm {
 
   private CostDistr problemCostDistr;
+  private int populationSize;
+  private SolutionSet population;
+  int evaluations;
+  private static String problemPath = "C:\\Users\\emine\\IdeaProjects\\MOEAD test project\\";
 
   /**
   * Constructor
   * @param problem Problem to solve
   */
-  public DE_CostDistr(Problem problem){
+  public DE_CostDistr(Problem problem, String dataPath){
     super(problem);
     problemCostDistr = (CostDistr) problem;
+    if (!dataPath.equals("-")) { problemPath = dataPath; }
   } // gDE
 
-  private int populationSize;
-  private SolutionSet population;
-  int evaluations;
-
-  /**   
+  /**
    * Runs of the DE algorithm.
    * @return a <code>SolutionSet</code> that is a set of non dominated solutions
    * as a result of the algorithm execution  
@@ -62,111 +65,139 @@ public class DE_CostDistr extends Algorithm {
 
      int maxEvaluations ;
 
-     SolutionSet offspringPopulation ;
-          
-     Operator selectionOperator ;
-     Operator crossoverOperator ;
-               
-     Comparator  comparator ;
-     
-     // Differential evolution parameters
-     int r1    ;
-     int r2    ;
-     int r3    ;
-     int jrand ;
+     Operator    mutationOperator  ;
+     Operator    crossoverOperator ;
+     Operator    selectionOperator ;
 
-     Solution parent[] ;
-     
-     //Read the parameters
+     Comparator  comparator        ;
+
+     // Read the params
      populationSize = ((Integer)this.getInputParameter("populationSize")).intValue();
-     maxEvaluations  = ((Integer)this.getInputParameter("maxEvaluations")).intValue();
+     maxEvaluations = ((Integer)this.getInputParameter("maxEvaluations")).intValue();
+
+     // Initialize the variables
+     population          = new SolutionSet(populationSize) ;
+
+     evaluations  = 0;
 
      // Read the operators
      comparator = (Comparator) this.getInputParameter("comparator");
-     selectionOperator = operators_.get("selection");   
-     crossoverOperator = operators_.get("crossover") ;
-     
-     //Initialize the variables
-     population  = new SolutionSet(populationSize);
-
-     /*
-     // Create the initial solutionSet
-     Solution newSolution;
-     for (int i = 0; i < populationSize; i++) {
-       newSolution = new Solution(problem_);                    
-       problem_.evaluate(newSolution);            
-       problem_.evaluateConstraints(newSolution);
-       evaluations++;
-       population.add(newSolution);
-     } //for
-
-      */
+     mutationOperator  = this.operators_.get("mutation");
+     crossoverOperator = this.operators_.get("crossover");
+     selectionOperator = this.operators_.get("selection");
 
      initPopulation();
-   
-     // Generations ...
+
+     // Sort population
      population.sort(comparator) ;
 
      //Convergence
      int generations_left_for_convergence = 10;
      int converged = generations_left_for_convergence;
      double best_solution = population.get(0).getObjective(0);
+     System.out.println("BEST SOL: " + best_solution);
+
+     int stop = 0;
+     int generation = 0;
 
      while (evaluations < maxEvaluations && converged != 0) {
-       
-       // Create the offSpring solutionSet      
-       offspringPopulation  = new SolutionSet(populationSize);        
 
-       // optional: keep best solution
-       //offspringPopulation.add(new Solution(population.get(0))) ;
+       Solution winner = population.get(0);
+       //System.out.println("q: " + winner.getUL_Optimism());
+
+       SolutionSet winnerPareto = winner.getLL_Pareto_pop();
+       winnerPareto.printParetoToFile(problemPath + "results\\Paretos\\PARETO_" + (generation), false);
+       SolutionSet onlyWinner = new SolutionSet(1);
+       onlyWinner.add(winner);
+       onlyWinner.printSelfConsumptionToFile(problemPath + "results\\SelfsInProgress\\SELF_" + (generation), false);
+
+       generation++;
+
+       SolutionSet offspringPopulation = new SolutionSet(populationSize) ;
+
+       double diff = Math.abs(winner.getObjective(0) - population.get(populationSize-1).getObjective(0));
+       if (diff <= 10 ) {
+         MOEAD.conv = 0.001;
+       }
+       if (diff <= 5 ) {
+         MOEAD.conv = 0.0001;
+       }
       
        for (int i = 0; i < populationSize; i++) {
          Solution current = population.get(i);
 
          // Selection: 3 random parents
          // Two parameters are required: the population and the index of the current individual
-         parent = (Solution [])selectionOperator.execute(new Object[]{population, i});
+         Solution[] tempParents = (Solution [])selectionOperator.execute(new Object[]{population, i});
+         Solution[] parents = new Solution[5];
+         parents[0] = tempParents[0]; parents[1] = tempParents[1]; parents[2] = tempParents[2];
+         parents[3] = tempParents[3]; parents[4] = winner;
 
-         Solution child ;
-         
          // Crossover.
          // Two parameters are required: the current individual and the array of parents
-         child = (Solution)crossoverOperator.execute(new Object[]{current, parent}) ;
+         Solution offspring = (Solution)crossoverOperator.execute(new Object[]{current, parents}) ;
 
-         problem_.evaluate(child) ;
+         // Mutation
+         mutationOperator.execute(offspring);
 
-         evaluations++ ;
+         //attach to offspring the LL ND of the closest UL solution
+         double min_dist = Double.MAX_VALUE;
+         int min_index = -1;
 
-         // if child better than currently-examined individual, replace him
-         if (comparator.compare(current, child) < 0)
-           offspringPopulation.add(new Solution(current)) ;
-         else
-           offspringPopulation.add(child) ;
-       }
-       
-       // The offspring population becomes the new current population
-       population.clear();
-       for (int i = 0; i < populationSize; i++) {
-         population.add(offspringPopulation.get(i)) ;
-       }
+         Double[] offspringVar = (((ArrayReal) offspring.getDecisionVariables()[0]).array_);
+         for (int j = 0; j < populationSize; j++) {
+           Double[] popSolutionVar = (((ArrayReal) population.get(j).getDecisionVariables()[0]).array_);
+           double dist = EuclideanDist.distance(offspringVar, popSolutionVar);
+           if (dist < min_dist){
+             min_dist = dist;
+             min_index = j;
+           }
+         }
+         offspring.setLL_Transfer_pop(population.get(min_index).getLL_Transfer_pop());
 
+         // Evaluation of the new individuals
+         problem_.evaluate(offspring);
+         evaluations++;
+
+         offspringPopulation.add(offspring);
+
+         if (stop == 1){
+           break;
+         }
+
+       } // for
+
+       // The offspring population is added to the new current population
+       SolutionSet popCombined = population.union(offspringPopulation);
        offspringPopulation.clear();
-       population.sort(comparator) ;
+       population.clear();
+       popCombined.sort(comparator);
+       //TODO add here more than just sorting....
+       for (int i = 0; i < populationSize; i++) {
+         population.add(popCombined.get(i)) ;
+       }
 
+       if (stop == 1){
+         break;
+       }
+
+       winner = population.get(0);
        //check for convergence
-       if (population.get(0).getObjective(0) >= best_solution){
+       if (winner.getObjective(0) > best_solution - 1){
          converged--;
+         //flag = PseudoRandom.randInt(1,3);
        } else {
-         best_solution = population.get(0).getObjective(0);
+         best_solution = winner.getObjective(0);
+         System.out.println("BEST SOL at " + evaluations + ": " + best_solution);
          converged = generations_left_for_convergence;
        }
 
      } // while
-     
+
      // Return a population with the best individual
      SolutionSet resultPopulation = new SolutionSet(1) ;
      resultPopulation.add(population.get(0)) ;
-     
+
      System.out.println("Evaluations: " + evaluations ) ;
      FileWriter evalsWriter = null;
      try {
@@ -177,7 +208,7 @@ public class DE_CostDistr extends Algorithm {
        e.printStackTrace();
      }
 
-     return resultPopulation ;
+     return resultPopulation;
    } // execute
 
   /**
